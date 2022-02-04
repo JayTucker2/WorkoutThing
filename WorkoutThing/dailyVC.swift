9411abdc1e0d5521d5ced78767b0736654f9cebe
JayTucker2 <prosnipa1221@gmail.com>
2022-02-04 08:09:51 -0600

    Date sorter and formatter

--- WorkoutThing/dailyVC.swift
+++ WorkoutThing/dailyVC.swift
@@ -13,9 +13,23 @@ class dailyVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var tableOutlet: UITableView!
     var dates : [String] = []
+    let dateArray = ["1 February, 2022", "2 February, 2022", "3 February, 2022"]
+    var convertedArray : [Date] = []
+    var dateFormatter = DateFormatter()
+    
     
     let date = Date()
     override func viewDidLoad() {
+        dateFormatter.dateFormat = "dd MM, yyyy"
+        
+        for dat in dateArray {
+            var date = dateFormatter.date(from: dat)
+            if let date = date{
+                convertedArray.append(date)
+            }
+        }
+        var ready = convertedArray.sorted(by: {$0.compare($1) == .orderedDescending})
+        print(ready)
         dates.append("2/1")
         super.viewDidLoad()
         
@@ -28,7 +42,9 @@ class dailyVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableOutlet.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         cell.textLabel?.text = dates[indexPath.row]
+        cell.detailTextLabel?.text = "Hi"
         return cell
     }
+
 
 }
